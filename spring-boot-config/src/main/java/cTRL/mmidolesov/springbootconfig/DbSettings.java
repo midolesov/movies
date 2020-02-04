373dd1c64bed53bc345f836039fdec284c8003e9
mmidolesov2 <m_midolesov@abv.bg>
2020-02-04 23:55:33 +0200

    Add update on greeting controllerand application.yaml

--- spring-boot-config/src/main/java/cTRL/mmidolesov/springbootconfig/DbSettings.java
+++ spring-boot-config/src/main/java/cTRL/mmidolesov/springbootconfig/DbSettings.java
@@ -7,6 +7,9 @@ import org.springframework.context.annotation.Configuration;
 @ConfigurationProperties("db")
 public class DbSettings {
 
+    /*This ConfigurationProperties annotation maps the variables below
+    to the, 'db' starting with properties, in application.properties/yaml
+    */
     private String connection;
     private String host;
     private int port;
--- spring-boot-config/src/main/java/cTRL/mmidolesov/springbootconfig/GreetingController.java
+++ spring-boot-config/src/main/java/cTRL/mmidolesov/springbootconfig/GreetingController.java
@@ -2,8 +2,11 @@ package cTRL.mmidolesov.springbootconfig;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class GreetingController {
@@ -11,12 +14,23 @@ public class GreetingController {
     @Value("${my.greeting}")
     private String greetingMessage;
 
+    @Value("${my.list.values}") 
+    private List<String> listValues;
+
     @Autowired
     private DbSettings dbSettings;
 
+    @Autowired
+    public Environment env;
+
     @GetMapping("/greeting")
     public String greeting(){
-        return dbSettings.getConnection() + " " + dbSettings.getHost() + " " + dbSettings.getPort();
+        return listValues + dbSettings.getConnection();
+    }
+
+    @GetMapping("/envdetails")
+    public String envDetals(){
+        return env.getProperty("");
     }
 
 }
