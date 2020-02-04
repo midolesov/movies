67a13a4cf414efdd39f1b7441444c599b424f463
mmidolesov2 <m_midolesov@abv.bg>
2020-02-04 23:55:48 +0200

    Add update on greeting controllerand application.yaml

--- spring-boot-config/src/main/java/cTRL/mmidolesov/springbootconfig/GreetingController.java
+++ spring-boot-config/src/main/java/cTRL/mmidolesov/springbootconfig/GreetingController.java
@@ -2,6 +2,7 @@ package cTRL.mmidolesov.springbootconfig;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RefreshScope
 public class GreetingController {
 
     @Value("${my.greeting}")
@@ -25,7 +27,7 @@ public class GreetingController {
 
     @GetMapping("/greeting")
     public String greeting(){
-        return listValues + dbSettings.getConnection();
+        return greetingMessage;
     }
 
     @GetMapping("/envdetails")
